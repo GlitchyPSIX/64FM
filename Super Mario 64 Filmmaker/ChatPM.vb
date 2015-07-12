065b0549ed9d82715aee300cf2ec60e00e83d5ac
GlitchyPSIX <supermglitchy3@gmail.com>
2015-07-11 22:37:12 -0400

    Fixed a image, a bug and added a notification when you get a PM with a window closed in CHAT

--- Super Mario 64 Filmmaker/ChatPM.vb
+++ Super Mario 64 Filmmaker/ChatPM.vb
@@ -1,21 +1,41 @@
-﻿Imports TechLifeForum
-Public Class ChatPM
-    Friend WithEvents irc As IrcClient
-    Public UserPM As String
-    Private Sub ChatPM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        irc = New IrcClient("irc.mibbit.com", 6667)
-    End Sub
+﻿
+Imports TechLifeForum
 
-    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
-        irc.SendMessage(UserPM, tbMessage.Text)
-        rtbLog.AppendText(irc.Nick + ":" & vbTab & tbMessage.Text + vbCrLf)
-        tbMessage.Clear()
-        tbMessage.Focus()
-        rtbLog.ScrollToCaret()
-    End Sub
+'Public Class ChatPM
+'    Friend WithEvents irc As IrcClient
+'    Public UserPM As String
+'    Public MessagePM As String
+'    Private Sub ChatPM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+'    End Sub
+'    Friend Sub New(User As String)
 
-    Private Sub irc_ChannelMessage(Channel As String, User As String, Message As String) Handles irc.ChannelMessage
-        rtbLog.AppendText(User & vbTab + Message & vbNewLine)
-        rtbLog.ScrollToCaret()
-    End Sub
-End Class
+'        ' This call is required by the designer.
+'        InitializeComponent()
+'        Me.Text = User
+'        UserPM = User
+
+'        ' Add any initialization after the InitializeComponent() call.
+
+'    End Sub
+'    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+'        Chat.irc.SendMessage(UserPM, tbMessage.Text)
+'        rtbLog.AppendText(Chat.irc.Nick + ":" & vbTab & tbMessage.Text + vbCrLf)
+'        tbMessage.Clear()
+'        tbMessage.Focus()
+'        rtbLog.ScrollToCaret()
+'    End Sub
+
+'    Private Sub irc_Message(sender As Object, e As PrivateMessageEventArgs) Handles irc.PrivateMessage
+'        If FormWindowState.Minimized Then
+'            Dim Notif As New NotiForm(e.From, e.Message)
+'            Notif.Show()
+'        Else
+'            If Me.Text = e.From Then
+'                rtbLog.AppendText(UserPM & vbTab + e.Message & vbNewLine)
+'                rtbLog.ScrollToCaret()
+'            Else
+'                'GlitchyPSI: Hey! That means this conversation is NOT WITH YOU!
+'            End If
+'        End If
+'    End Sub
+'End Class
